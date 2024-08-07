3310ffbab5032731e7d9a2c0d474db141780f773
prat <chawdapratham31@gmail.com>
2024-08-07 15:32:48 +0530

    Add files via upload

---
+++ 2. operators.py
@@ -0,0 +1,8 @@
+print(2+4)  # addition operator
+print(2*4)  # multiplication operartor
+print(2-4)  # subtraction operator
+print(2/4)  # division operator
+print(2**4) # exponential operator
+print(2%4)  # modulus operator
+print(2//4) #floor division operator
+
